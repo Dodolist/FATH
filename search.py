758f1ff652b7a5aa788339e8ceab38b607f7a08e
Dodolist <dodolist@naver.com>
2021-01-03 21:29:40 +0900

    set vowel_arr

--- search.py
+++ search.py
@@ -1,3 +1,17 @@
+vowelAlphabet=['k','i','j','u','h','y','n','b','m','l','p','o','P','O']
+
 search = input("검색창: ")
 
 print('출력: '+search)
+
+input_length = len(search)
+
+vowel_arr = [0 for i in range(input_length)]
+consonant_arr = [1 for i in range(input_length)]
+
+for i in range(input_length):
+    if search[i] in vowelAlphabet:
+        vowel_arr[i] = 1
+    pass
+
+print(vowel_arr)
